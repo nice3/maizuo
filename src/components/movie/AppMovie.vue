2adcf9f23d2daa222978a8ce4cff587683cd0bcc
nice3 <1032878312@qq.com>
2018-01-30 14:44:47 +0800

    mz

---
+++ src/components/movie/AppMovie.vue
@@ -0,0 +1,15 @@
+
+<template>
+    <div class="app-movie app-box">
+        movie
+    </div>
+</template> 
+
+<script>
+    export default {
+        name:'app-movie'
+    }
+</script>
+<style >
+
+</style>
